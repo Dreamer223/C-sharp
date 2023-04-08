7369688ee8571759dea8125e7cb07fd221da63a1
Dreamer223 <drackfil@gmail.com>
2023-04-08 12:30:14 +0300

    4 задание плюс решение ChatGPT он мне не много помог понять где я сделал ошибку

--- Home_work_Task1/Program.cs
+++ Home_work_Task1/Program.cs
@@ -66,5 +66,51 @@
 //     Console.Write("Число не является четным");
 // }
 
+// 4 Задание.
+Console.WriteLine("4 Задание");
+Console.Write("Введите число: ");
+string number1 = Console.ReadLine();
+int n = Convert.ToInt32(number1);
+Console.Write("Четные числа от 1 до {0}:", n);
+int i = 2;
 
+while(i <= n)
+{
+    Console.Write("{0} ", i);
+    i += 2;
+
+}
+
+
+// Это как решил ChatGPT не понятно и сложно пока что
+
+  // Console.Write("Введите число: ");
+        // int n = int.Parse(Console.ReadLine());
+
+        // Console.WriteLine("Четные числа от 1 до {0}:", n);
+
+        // int i = 2; // начинаем с 2 (первое четное число)
+        // while (i <= n) { // пока i не превысило заданное число n
+        //     Console.Write("{0} ", i);
+        //     i += 2; // увеличиваем i на 2 на каждой итерации
+        // }
+
+        // Console.ReadLine();
+
+// using System;
+
+// class Program {
+//     static void Main(string[] args) {
+//         Console.Write("Введите число: ");
+//         int n = int.Parse(Console.ReadLine());
+
+//         Console.WriteLine("Четные числа от 1 до {0}:", n);
+//         for (int i = 2; i <= n; i += 2) {
+//             Console.Write("{0} ", i);
+//         }
+//         Console.ReadLine();
+//     }
+// }
+   
+      
     

fb4fd94c4b641553ae99025b93a099d48bcd66ae
Dreamer223 <drackfil@gmail.com>
2023-04-08 10:47:08 +0300

    1 Задание

---
+++ Home_work_Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// 1 Задание
+Console.WriteLine("1 Задание")
+Console.Write("Введите число: ");
+string number1 = Console.ReadLine();
+int numberA = Convert.ToInt32(number1);
+Console.Write("Введите число: ");
+string number2 = Console.ReadLine();
+int numberB = Convert.ToInt32(number2);
+if(numberA >  numberB)
+{
+    Console.WriteLine("Число А больше числа В");
+}
+else
+{
+    Console.WriteLine("Число B больше числа A");
+}
+
+// 2 Задание

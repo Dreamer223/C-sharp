383632ba89fb1baea9e3c365f5bad10127a996c8
Dreamer223 <drackfil@gmail.com>
2023-04-08 12:28:42 +0300

    2 задание

--- Home_work_Task1/Program.cs
+++ Home_work_Task1/Program.cs
@@ -1,18 +1,54 @@
 ﻿// 1 Задание
-Console.WriteLine("1 Задание")
-Console.Write("Введите число: ");
-string number1 = Console.ReadLine();
-int numberA = Convert.ToInt32(number1);
-Console.Write("Введите число: ");
-string number2 = Console.ReadLine();
-int numberB = Convert.ToInt32(number2);
-if(numberA >  numberB)
-{
-    Console.WriteLine("Число А больше числа В");
-}
-else
-{
-    Console.WriteLine("Число B больше числа A");
-}
+// Console.WriteLine("1 Задание");
+// Console.Write("Введите число: ");
+// string number1 = Console.ReadLine();
+// int numberA = Convert.ToInt32(number1);
+// Console.Write("Введите число: ");
+// string number2 = Console.ReadLine();
+// int numberB = Convert.ToInt32(number2);
+// if(numberA >  numberB)
+// {
+//     Console.WriteLine("Число А больше числа В");
+// }
+// else
+// {
+//     Console.WriteLine("Число B больше числа A");
+// }
 
 // 2 Задание
+// Console.WriteLine("2 Задание");
+
+// Console.Write("Введите число: ");
+// string number1 = Console.ReadLine();
+// int numberA = Convert.ToInt32(number1);
+// Console.Write("Введите число: ");
+// string number2 = Console.ReadLine();
+// int numberB = Convert.ToInt32(number2);
+// Console.Write("Введите число: ");
+// string number3 = Console.ReadLine();
+// int numberC = Convert.ToInt32(number3);
+// int max = numberA;
+// if(numberA > numberB)
+// {
+//     if(numberA > numberC)
+//     {
+//         Console.WriteLine("А максимальное число");
+//     }
+//     else
+//     {
+//         Console.WriteLine("C максимальное число ");
+//     }
+
+// }
+// else
+// {
+//     if(numberB > numberC)
+//     {
+//         Console.WriteLine("В максимальное число ");
+//     }
+//     else
+//     {
+//         Console.WriteLine("C максимальное число ");
+//     }
+// }
+

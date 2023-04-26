df81b786b8bc7a9a3a76ddd3f6b50322ff094cb0
Dreamer223 <drackfil@gmail.com>
2023-04-26 10:57:21 +0300

    HomeWorkTask 5

--- HomeWorkTask5/Program.cs
+++ HomeWorkTask5/Program.cs
@@ -21,7 +21,7 @@
 //     {
 //         Console.Write("[" + array[i] + "]");
 //     }
-  
+
 // }
 // void ChetIndexArray()
 // {
@@ -70,7 +70,7 @@
 //         Console.Write("[" + array[i] + "]");
 //     }
 //     return;
-  
+
 // }
 
 // void SummNechetIndexNumber()
@@ -96,6 +96,66 @@
 
 // [3 7 22 2 78] -> 76
 
+// Console.WriteLine("Введите длинну массива: ");
+// int[] array = new int[int.Parse(Console.ReadLine())];
+// int max = 0;
+// int min = 0;
+// void FillArray()
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(-100, 101);
+
+//     }
+//     return;
+// }
+// void PrintArray()
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write("[" + array[i] + "]");
+//     }
+//     return;
+
+// }
+
+// void MaxElements()
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > max)
+//         {
+//             max = array[i];
+//         }
+//     }
+// }
+
+// void MinElements()
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] < min)
+//         {
+//             min = array[i];
+//         }
+//     }
+// }
+
+// void Difference()
+// {
+//     int difference = max - min;
+//     Console.WriteLine($"Разница между максимальным числом {max} и минимальным числом {min} = {difference}");
+// }
+
+
+// FillArray();
+// PrintArray();
+// Console.WriteLine();
+// MaxElements();
+// MinElements();
+// Difference();
+
+
 
 // Доп. задачи
 // Задача 3: Задайте массив из 8 случайных чисел из промежутка [-9, 9]. 
@@ -105,6 +165,53 @@
 // 4; массив [6, 7, 19, 345, 3] -> нет
 
 // 3; массив [6, 7, 19, 345, 3] -> да
+
+
+// Console.WriteLine("Введите длинну массива: ");
+// int[] array = new int[int.Parse(Console.ReadLine())];
+// void FillArray()
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(-9, 9);
+
+//     }
+//     return;
+// }
+// void PrintArray()
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write("[" + array[i] + "]");
+//     }
+//     return;
+
+// }
+// Console.WriteLine("Введите ваше число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] == b)
+//     {
+//         b = array[i];
+//         Console.WriteLine($"Есть соответствие с вашим числом {b}");
+//         break;
+//     }
+//     // else
+//     // {
+//     // Console.WriteLine("Не нашлось соответсвующего элемента с вамши числом в этом массиве");
+//     // break;
+//     // }
+// }
+// // Console.WriteLine("Не нашлось соответсвующего элемента с вамши числом в этом массиве");
+
+
+// FillArray();
+
+// PrintArray();
+
+// //  НЕ ПОНИМАЮ КАК СДЕЛАТЬ ВРОДЕ ВСЕ ПРАВИЛЬНО, НО ВСЕ РАВНО НЕ ПРАВИЛЬНО //
+// // Elemunts();
 
 // Задача 4: Задайте массив из 10 случайных чисел из промежутка [-100, 100]. 
 // Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
@@ -116,3 +223,23 @@
 // [1, 2, 3, 6, 2] -> 0
 
 // [10, 11, 12, 13, 14] -> 5
+int[] array = new int[int.Parse(Console.ReadLine())];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("[" + array[i] + "]");
+}
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] >= 10 && array[i] <= 99)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"В в этом массиве в промежутке от 10 до 99 {count} чисел");
